6353ab2dad69cae213b34cd60165a639f9d0f1e1
Daniel Kováč <dado.kovac04@gmail.com>
2021-11-12 09:46:08 +0100

    colors

--- To-Do/src/router/index.ts
+++ To-Do/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue'
 import Lists from '../views/Lists.vue'
+import Todo from '../views/Todo.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/lists',
     name: 'Lists',
     component: Lists
+  },
+  {
+    path: '/todo',
+    name: 'Todo',
+    component: Todo
   },
   {
     path: '/home',
---
+++ To-Do/src/views/Todo.vue
@@ -0,0 +1,65 @@
+<template>
+	<ion-page>
+		<div>
+			<ion-icon name="chevron-back-outline" size="large" class="backicon"></ion-icon>
+			<ion-icon name="trash-outline" size="large" class="trashicon"></ion-icon>
+		</div>
+
+		<div>
+			<h1>Monday dinner</h1>
+		</div>
+
+		<div>
+			<ion-list>
+				<ion-item>
+					<ion-checkbox></ion-checkbox>
+					<ion-label>Airline</ion-label>
+					<ion-label>3 kg</ion-label>
+				</ion-item>
+				<ion-item>
+					<ion-checkbox></ion-checkbox>
+					<ion-label>Airline</ion-label>
+					<ion-label>3 kg</ion-label>
+				</ion-item>
+				<ion-item>
+					<ion-checkbox></ion-checkbox>
+					<ion-label>Airline</ion-label>
+					<ion-label>3 kg</ion-label>
+				</ion-item>
+			</ion-list>
+		</div>
+	</ion-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { IonPage,IonIcon,IonList,IonItem,IonCheckbox,IonLabel } from '@ionic/vue';
+import { chevronBackOutline,trashOutline } from 'ionicons/icons';
+
+export default defineComponent({
+
+	components:{
+		IonPage,IonIcon,IonList,IonItem,IonCheckbox,IonLabel
+	},
+
+	setup(){
+
+		return{
+
+		}
+	}
+
+})
+</script>
+
+<style>
+
+.backicon{
+	color: lightskyblue;
+}
+
+.trashicon{
+	color: red;
+}
+
+</style>
