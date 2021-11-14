165a71e9f7943da7e7180a2127a05ea29cc467a9
Og1S <dado.kovac04@gmail.com>
2021-11-14 19:32:46 +0100

    gg

--- To-Do/src/views/Todo.vue
+++ To-Do/src/views/Todo.vue
@@ -1,15 +1,14 @@
 <template>
 	<ion-page>
-		<div>
-			<ion-icon :icon="chevronBackOutline" size="large" class="backicon"></ion-icon>
-			<ion-icon :icon="trashOutline" size="large" class="trashicon"></ion-icon>
-		</div>
+		<ion-header>
+			<ion-toolbar>
+				<ion-icon :icon="chevronBackOutline" size="large" class="backicon" slot="start"></ion-icon>
+				<ion-title>Monday dinner</ion-title>
+				<ion-icon :icon="trashOutline" size="large" class="trashicon" slot="end"></ion-icon>
+			</ion-toolbar>
+		</ion-header>
 
-		<div>
-			<h1>Monday dinner</h1>
-		</div>
-
-		<div>
+		<ion-content>
 			<ion-list>
 				<ion-item>
 					<ion-checkbox></ion-checkbox>
@@ -27,19 +26,19 @@
 					<ion-label>3 kg</ion-label>
 				</ion-item>
 			</ion-list>
-		</div>
+		</ion-content>
 	</ion-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { IonPage,IonIcon,IonList,IonItem,IonCheckbox,IonLabel } from '@ionic/vue';
+import { IonPage,IonIcon,IonList,IonItem,IonCheckbox,IonLabel,IonTitle,IonToolbar,IonHeader,IonContent } from '@ionic/vue';
 import { chevronBackOutline,trashOutline } from 'ionicons/icons';
 
 export default defineComponent({
 
 	components:{
-		IonPage,IonIcon,IonList,IonItem,IonCheckbox,IonLabel
+		IonPage,IonIcon,IonList,IonItem,IonCheckbox,IonLabel,IonTitle,IonToolbar,IonHeader,IonContent
 	},
 
 	setup(){
